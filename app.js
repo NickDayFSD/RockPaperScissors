23deb1fe4106f513ab5553ffda517b23c415e7f9
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-03-25 16:57:57 -0700

    imported functions, grabbed DOM elements, and initialized state

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 // import functions and grab DOM elements
+import {
+    throwSelector,
+    doesUserWin
+} from './utils.js'
+
+// buttons
+const throwButton = document.querySelector('#submit');
+const resetButton = document.querySelector('#reset');
+
+// outputs
+const resultsEl = document.querySelector('#results');
+const winsEl = document.querySelector('#total-wins');
+const lossEl = document.querySelector('#total-losses');
+const drawEl = document.querySelector('#total-draws');
+const totalsEl = document.querySelector('#total-throws');
+
+// inputs
+const userThrow = document.querySelector('input: checked');
 
 // initialize state
+let wins = 0;
+let losses = 0;
+let ties = 0;
+let total = wins + ties + losses;
 
 // set event listeners to update state and DOM
+throwButton.addEventListener('click', () {
+    const robotThrow = throwSelector();
+    console.log(robotThrow);
+
+})
--- utils.js
+++ utils.js
@@ -13,7 +13,6 @@ export function doesUserWin(randomThrow, userThrow) {
     if (randomThrow === userThrow) {
         return 'tie';
     } else if ((randomThrow === 'rock' && userThrow === 'paper') || (randomThrow === 'paper' && userThrow === 'scissors') || (randomThrow === 'scissors' && userThrow === 'rock')) {
-        //win++
         return 'win';
     } else {
         return 'lose';
