cc59706a73669276d72b9f18803ca7f01edba572
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-03-25 17:56:17 -0700

    completed throw button and display results

--- app.js
+++ app.js
@@ -15,18 +15,34 @@ const lossEl = document.querySelector('#total-losses');
 const drawEl = document.querySelector('#total-draws');
 const totalsEl = document.querySelector('#total-throws');
 
-// inputs
-const userThrow = document.querySelector('input: checked');
-
 // initialize state
 let wins = 0;
 let losses = 0;
 let ties = 0;
-let total = wins + ties + losses;
 
 // set event listeners to update state and DOM
-throwButton.addEventListener('click', () {
+throwButton.addEventListener('click', () => {
     const robotThrow = throwSelector();
-    console.log(robotThrow);
+    const userRadioSelection = document.querySelector('input:checked');
+    const userThrow = userRadioSelection.value;
+    console.log('robot chose ', robotThrow);
+    const result = doesUserWin(robotThrow, userThrow);
+    console.log('user chose', userThrow);
+    console.log('result is...', result);
+
+    // increment results
+    if (result === 'tie') {
+        ties++;
+    } else if (result === 'lose') {
+        losses++;
+    } else {
+        wins++;
+    };
+
+    resultsEl.textContent = `Your ${userThrow} vs my ${robotThrow} means you ${result}!`;
+    winsEl.textContent = `Wins = ${wins}`;
+    lossEl.textContent = `Losses = ${losses}`;
+    drawEl.textContent = `Ties = ${ties}`;
+    totalsEl.textContent = `Total Games = ${wins + losses + ties}`;
 
 })
